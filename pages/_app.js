fb74f4be43821d84f8e08161b3883e42dec3773a
pr-morteza <95617841+pr-morteza@users.noreply.github.com>
2023-02-24 21:03:16 +0330

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
-
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { createTheme, ThemeProvider } from '@mui/material'
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [ 'Barlow Semi Condensed', 'sans-serif'].join(',')
+   }
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider>
 }
 
 export default MyApp
